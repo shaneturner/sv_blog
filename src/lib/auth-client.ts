24dc60fdecd7762b3327236713c289be18f4dfa0
Shane Turner <shaneturner@users.noreply.github.com>
2025-12-23 17:22:27 +1300

    WIP

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,7 @@
+import { adminClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/svelte";
+
+
+export const authClient = createAuthClient({
+  plugins: [adminClient()],
+});
